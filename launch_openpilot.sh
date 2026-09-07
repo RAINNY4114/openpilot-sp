5acfba16330c3112f39376e64fc32c2aa5711ded
RAINNY4114 <152569703+RAINNY4114@users.noreply.github.com>
2026-09-07 14:19:35 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
+# ==========================
+export ATHENA_HOST='wss://athena.konik.ai/'
+export API_HOST='https://api.konik.ai'
+export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
+export SKIP_FW_QUERY=1
+export NAVD=true
+export NAVMODEL=true
+
+# ==========================
+
+export FINGERPRINT="FORD_EXPLORER_MK6"
+export SKIP_FW_QUERY="1"
+
 exec ./launch_chffrplus.sh
