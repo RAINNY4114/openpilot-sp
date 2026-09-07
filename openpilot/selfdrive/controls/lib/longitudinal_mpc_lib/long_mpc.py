645fd077f7e84302e24761928638557b0acb11d7
RAINNY4114 <152569703+RAINNY4114@users.noreply.github.com>
2026-09-07 14:14:27 +0800

    Update long_mpc.py

--- openpilot/selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ openpilot/selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -54,14 +54,14 @@ T_IDXS = np.array(T_IDXS_LST)
 FCW_IDXS = T_IDXS < 5.0
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 COMFORT_BRAKE = 2.5
-STOP_DISTANCE = 6.0
+STOP_DISTANCE = 3.0
 MIN_X_LEAD_FACTOR = 0.5
 
 def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
     return 1.0
   elif personality==log.LongitudinalPersonality.standard:
-    return 1.0
+    return 0.8
   elif personality==log.LongitudinalPersonality.aggressive:
     return 0.5
   else:
@@ -72,9 +72,9 @@ def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
     return 1.75
   elif personality==log.LongitudinalPersonality.standard:
-    return 1.45
-  elif personality==log.LongitudinalPersonality.aggressive:
     return 1.25
+  elif personality==log.LongitudinalPersonality.aggressive:
+    return 0.85
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
